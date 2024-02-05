27c5003e64ca343ea234116bfa5a21de59b0fca5
jacopo-derrico <derricoj.design@gmail.com>
2024-02-05 17:28:25 +0100

    generate and print emails in one batch

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        
-      }
+        return {
+            randomEmails: [],
+        }
+    },
+    methods: {
+        generateEmail() {
+            this.randomEmails = []
+
+            for (let i = 0; i < 10; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    console.log(result.data)
+                    this.randomEmails.push(result.data.response)
+                })
+
+            }
+        }
     }
-  }).mount('#app')
+}).mount('#app')
